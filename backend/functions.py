dcc225c166dd38d51c58601e9f2ca36558b1598c
Rohit Dham <rdhammpls@gmail.com>
2024-09-08 17:01:07 -0500

    re test?

--- backend/functions.py
+++ backend/functions.py
@@ -2,19 +2,19 @@ import ctypes
 import os
 
 # Load the shared library
-# script_dir = os.path.dirname(os.path.abspath(__file__))
-# lib_path = os.path.join(script_dir, 'libfactorial.so')
-# lib = ctypes.CDLL(lib_path)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+lib_path = os.path.join(script_dir, 'libfactorial.so')
+lib = ctypes.CDLL(lib_path)
 
-# lib.factorial.argtypes = [ctypes.c_int]
-# lib.factorial.restype = ctypes.c_ulonglong
+lib.factorial.argtypes = [ctypes.c_int]
+lib.factorial.restype = ctypes.c_ulonglong
 
 def factorial(num):
-    if num == 0 or num == 1:
-        return num
-    else:
-        return num * factorial(num-1)
-    # return lib.factorial(num)
+    # if num == 0 or num == 1:
+    #     return num
+    # else:
+    #     return num * factorial(num-1)
+    return lib.factorial(num)
 
 if __name__ == '__main__':
     print(factorial(5))
